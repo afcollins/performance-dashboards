7d99699571a9e56aee062caba56f72ba16a494e6
Andrew Collins <ancollin@redhat.com>
2026-05-14 17:31:35 -0700

    f - replace interval variable
    Signed-off-by: Andrew Collins <ancollin@redhat.com>

--- go/query_tracker.go
+++ go/query_tracker.go
@@ -8,13 +8,18 @@ import (
 	mg "github.com/kube-burner/metrics-generator/pkg/metrics"
 )
 
+// profileInterval is the default rate interval used in metrics profile queries.
+const profileInterval = mg.Rate2m
+
 // grafanaVarRe matches label matchers whose value contains a Grafana template variable ($...).
 // These are dashboard-only scoping — stripped before registering in the metrics profile generator.
 var grafanaVarRe = regexp.MustCompile(`,?\s*\w+[=!~]+=?"[^"]*\$[^"]*"`)
 
 func stripGrafanaVars(expr string) string {
 	clean := grafanaVarRe.ReplaceAllString(expr, "")
-	return strings.ReplaceAll(clean, "{}", "")
+	clean = strings.ReplaceAll(clean, "{}", "")
+	clean = strings.ReplaceAll(clean, "$interval", string(profileInterval))
+	return clean
 }
 
 type queryTracker struct{ g *mg.Generator }
